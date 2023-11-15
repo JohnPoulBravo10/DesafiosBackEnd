033af7185b2f7d05fd54cafe03651bb25781340d
juan pablo bravo <juanpablobravo0209@gmail.com>
2023-11-14 21:24:44 -0300

    desafio2

---
+++ ProductManager.js
@@ -0,0 +1,95 @@
+import fs from 'fs';
+
+export default class ProductManager {
+  constructor(filePath) {
+    this.path = filePath;
+    this.products = [];
+    this.nextId = 1;
+
+    
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    if (title && description && price && thumbnail && code && stock) {
+      if (!this.products.find((product) => product.code === code)) {
+        const product = {
+          id: this.nextId++,
+          title: title,
+          description: description,
+          price: price,
+          thumbnail: thumbnail,
+          code: code,
+          stock: stock,
+        };
+        this.products.push(product);
+        await this.guardarProductos();
+      } else {
+        return "El código ya existe, elija otro código";
+      }
+    } else {
+      return "Complete todos los campos";
+    }
+  }
+
+  getProducts() {
+    this.cargarProductos()
+    return this.products;
+  }
+
+  getProductById(id) {
+    this.cargarProductos()
+    const product = this.products.find((prod) => prod.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.log("Not found");
+      return null;
+    }
+  }
+
+  async updateProduct(id, updatedProduct) {
+    this.cargarProductos()
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      updatedProduct.id = id;
+      this.products[index] = updatedProduct;
+      await this.guardarProductos();
+      return this.products[index];
+    } else {
+      return "Producto no encontrado";
+    }
+  }
+
+  async deleteProduct(id) {
+    this.cargarProductos()
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      await this.guardarProductos();
+      return "Producto eliminado correctamente";
+    } else {
+      return "Producto no encontrado";
+    }
+  }
+
+  
+  async cargarProductos() {
+    try {
+      const data = await fs.promises.readFile(this.path, 'utf-8');
+      this.products = JSON.parse(data);
+      this.nextId =
+        this.products.length > 0
+          ? this.products[this.products.length] + 1
+          : 1;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+ 
+  async guardarProductos() {
+    await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, "\t"), 'utf-8');
+  }
+}
+
+
---
+++ index.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import  ProductManager  from './ProductManager.js'; 
+
+(async () => {
+  const filePath = 'productos.json'; 
+  const listaProductos = new ProductManager(filePath);
+
+
+  console.log("Productos al inicio:", listaProductos.getProducts());
+
+  
+  const addProductResult = await listaProductos.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+  );
+  console.log(addProductResult);
+
+ 
+  console.log("Productos después de agregar uno:", listaProductos.getProducts());
+
+
+  const productId = 1; 
+  try {
+    const productById = listaProductos.getProductById(productId);
+    console.log("Producto encontrado por ID:", productById);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  
+  const updatedProduct = {
+    title: "Producto Actualizado",
+    description: "Descripción Actualizada",
+    price: 250, 
+    thumbnail: "Nueva imagen",
+    code: "abc123",
+    stock: 30, 
+  };
+
+  try {
+    const updatedProductResult = await listaProductos.updateProduct(productId, updatedProduct);
+    console.log("Producto actualizado:", updatedProductResult);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  
+  try {
+    const deleteProductResult = await listaProductos.deleteProduct(productId);
+    console.log(deleteProductResult);
+    console.log("Productos después de eliminar uno:", listaProductos.getProducts());
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
